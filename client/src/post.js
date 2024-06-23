7bd9a94a1b6ca02b92c33a166ae65e3965782d3b
Aavash1738 <bhattaraiaavash1@gmail.com>
2024-06-23 20:00:26 +0545

    First push, formed layouts and managed login and register layouts

---
+++ client/src/post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return (
+        <div className = "post">
+            <div className="image">
+                <img src = "https://techcrunch.com/wp-content/uploads/2024/06/XREAL-Beam-Pro-Camera_2.jpeg?resize=768,512"></img>
+            </div>
+            <div className="texts>">
+                <h2>New battery bots make the market rumble</h2>
+                <p className="info">
+                    <a className="author">Aavash</a>
+                    <time>2024-06-21 22.57</time>
+                </p>
+                <p className="summary">This week at the AWE (Augmented World Expo) conference in Southern California, the company is showcasing the Beam Pro.</p>
+            </div>
+        </div>
+    )
+}
