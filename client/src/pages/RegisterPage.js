f29437aac098e862f8f8175bbe460e2bd389aedf
Aavash1738 <bhattaraiaavash1@gmail.com>
2024-07-04 08:41:55 +0545

    final modifications

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,9 +1,29 @@
+import {useState} from "react";
+
 export default function RegisterPage(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+async function register(ev){
+    ev.preventDefault(); //not trying to redirect from page
+    await fetch('http://localhost:4000', {
+        method: 'POST',
+        body: JSON.stringify({username, password}),
+        header: {'Content-Type':'application/json'}
+    })
+}   
+
     return(
-        <form className="register" action="">
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
-            <input type='text' placeholder="username"></input>
-            <input type="password" placeholder="password"></input>
+            <input type='text' 
+                   placeholder="username" 
+                   value={username}
+                   onChange={ev => setUsername(ev.target.value)}></input>
+            <input type="password" 
+                   placeholder="password" 
+                   value={password}
+                   onChange={ev => setPassword(ev.target.value)}></input>
             <button>Register</button>
         </form>
     )
